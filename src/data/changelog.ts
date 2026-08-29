7d22ee8a9f7a0e15989340bbca08dac4e3582211
R1CKzo <marley.nunes12345@gmail.com>
2026-08-29 14:53:23 -0300

    docs: anota push-to-talk nas novidades

--- src/data/changelog.ts
+++ src/data/changelog.ts
@@ -50,6 +50,7 @@ export const changelog: ChangelogEntry[] = [
     betaFeatures: [
       "Novo estilo de interface: \"liquid glass\" (Configurações > Beta), continua em teste",
       "Atalhos globais de chamada (app de desktop): Ctrl+Shift+M mutar, Ctrl+Shift+S silenciar geral, Ctrl+Shift+L sair da chamada",
+      "Push-to-talk configurável em Configurações > Atalhos (app de desktop)",
     ],
     bugsFixed: [
       "Cursor do mouse mal posicionado e perda de nitidez durante o compartilhamento de tela",
